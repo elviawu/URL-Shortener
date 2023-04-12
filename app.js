c9c896af777748d735507d28b4006d95e9dce2ea
elviawu <elviawu@gmail.com>
2023-04-12 16:31:06 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`The server is running on http://localhost:${port}`)
+})
